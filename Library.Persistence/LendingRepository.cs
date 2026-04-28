b553b3efb71176c37d3e8225851ebae7c6471d78
YbenBroeckaert-HG <263457303+YbenBroeckaert-HG@users.noreply.github.com>
2026-04-28 11:19:35 +0200

    Tweaks

--- Library.Persistence/LendingRepository.cs
+++ Library.Persistence/LendingRepository.cs
@@ -4,5 +4,29 @@ namespace Library.Persistence
 {
     public class LendingRepository : ILendingRepository
     {
+        public List<Lenders> GetAllLenders()
+        {
+            using SqlConnection connection = new("Data Source=.;Initial Catalog=Library;Integrated Security=True;Trust Server Certificate=True");
+            connection.Open();
+            using SqlCommand command = new("SELECT * FROM Lenders", connection);
+            List<Lenders> lenders = [];
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int LendersId = (int)reader["Id"];
+                        string name = (string)reader["Name"];
+
+                        Lenders lender = new(LendersId, name);
+                        lenders.Add(lender);
+
+                    }
+                }
+            }
+            return lenders;
+        }
     }
 }
