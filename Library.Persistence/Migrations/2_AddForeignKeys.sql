de9a271838d9d3f05a0101c11def802241513e3f
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2026-04-28 09:41:34 +0200

    Add project links

--- Library.Persistence/Migrations/2_AddForeignKeys.sql
+++ Library.Persistence/Migrations/2_AddForeignKeys.sql
@@ -1,1 +1,10 @@
-﻿
+﻿ALTER TABLE [Library].[dbo].[Books]
+
+	ADD [AuthorId] INT NOT NULL, 
+    CONSTRAINT [FK_Books_Authors] FOREIGN KEY ([AuthorId]) REFERENCES [Authors]([Id])
+
+	ALTER TABLE [Library].[dbo].[Lenders]
+
+	ADD [LendingId] INT NOT NULL, 
+    CONSTRAINT [FK_LendingsLenders] FOREIGN KEY ([LendingId]) REFERENCES [Lendings]([Id])
+
