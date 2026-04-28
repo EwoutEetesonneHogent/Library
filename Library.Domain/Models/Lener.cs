7d21412e8ec0edf2044837aa99a17fa5a4c27b8f
pgm-lucaamey <lucas.ameye@student.arteveldehs.be>
2026-04-28 11:59:48 +0200

    validation

--- Library.Domain/Models/Lener.cs
+++ Library.Domain/Models/Lener.cs
@@ -7,8 +7,22 @@ namespace Library.Domain.Models
 {
     public class Lener
     {
+        private int _id;
         private string _name;
+
+        public int Id { get { return _id; } }
         public string Name { get { return _name; } }
 
+        public Lener(int id, string name)
+        {
+            if (id <= 0)
+                throw new ArgumentException("Id moet groter zijn dan 0.", nameof(id));
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Naam mag niet leeg zijn.", nameof(name));
+
+            _id = id;
+            _name = name;
+        }
     }
 }
