fec5cf153607cf06330d77072991ebb6f057114f
TheQuantumMoon <114161742+TheQuantumMoon@users.noreply.github.com>
2026-04-28 09:42:38 +0200

    Little message

--- Library.Startup/Program.cs
+++ Library.Startup/Program.cs
@@ -3,6 +3,8 @@ using Library.Domain.Repository;
 using Library.Persistence;
 using Library.Presentation;
 
+// Arnout was here
+
 IBookRepository bookRepository = new BookRepository();
 ILendingRepository lendingRepository = new LendingRepository();
 
