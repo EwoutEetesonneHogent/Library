08608838649a34cb2c7dee4c357be07a99692af8
ThorstenDhondt <thorsten.dhondt@student.hogent.be>
2026-04-28 09:56:13 +0200

    Initial commit

---
+++ Library.Persistence/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Persistence.Models
+{
+    internal class Author
+    {
+        private string _name;
+
+        public Author(string name)
+        {
+            _name = name;
+        }
+    }
+}
