ae967fff00c963fe21d920d7ed3c60cd35aeb005
roosebrouckmaxim <roosebrouckmaxim@hotmail.com>
2026-04-28 10:20:13 +0200

    Added Book constructor

--- Library.Persistence/Models/Book.cs
+++ Library.Persistence/Models/Book.cs
@@ -10,11 +10,18 @@ namespace Library.Persistence.Models {
         private int _publicationYear;
         private Author _author;
 
+        public Book(int id, string title, int publicationYear, Author author) {
+            Id = id;
+            Title = title;
+            PublicationYear = publicationYear;
+            Author = author;
+        }
+
         public int Id { get => _id; set => _id = value; }
 
         public string Title { get => _title; set => _title = value; }
 
-        public int PublicationYear => _publicationYear;
+        public int PublicationYear { get => _publicationYear; set => _publicationYear = value; }
 
         public Author Author { get => _author; set => _author = value; }
     }
