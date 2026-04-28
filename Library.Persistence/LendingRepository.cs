cbd1c12b55ed1da0e2a5c3ee8285b7402ee9b5e6
YbenBroeckaert-HG <263457303+YbenBroeckaert-HG@users.noreply.github.com>
2026-04-28 11:27:06 +0200

    Added secret connectionstring

--- Library.Persistence/LendingRepository.cs
+++ Library.Persistence/LendingRepository.cs
@@ -12,5 +12,29 @@ namespace Library.Persistence
         {
             _connectionString = connectionString;
         }
+        public List<Lenders> GetAllLenders()
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+            using SqlCommand command = new("SELECT * FROM Lenders", connection);
+            List<Lenders> lenders = [];
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int LendersId = (int)reader["Id"];
+                        string name = (string)reader["Name"];
+
+                        Lenders lender = new(LendersId, name);
+                        lenders.Add(lender);
+
+                    }
+                }
+            }
+            return lenders;
+        }
     }
 }
