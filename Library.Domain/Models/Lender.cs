0654dbf4a74790df06a968aca00e28e85c093fb8
xnumix <231570876+xnumix@users.noreply.github.com>
2026-04-28 11:35:48 +0200

    Lener veranderd naar lender

--- Library.Domain/Models/Lender.cs
+++ Library.Domain/Models/Lender.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Library.Domain.Models
 {
-    public class Lener
+    public class Lender
     {
         private string _name;
         public string Name { get { return _name; } }
