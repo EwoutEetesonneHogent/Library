ec3a306a29add6f1b6f7b846a1cc32632954702d
roosebrouckmaxim <roosebrouckmaxim@hotmail.com>
2026-04-28 09:56:19 +0200

    Made Book model

---
+++ Library.Persistence/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Library.Persistence.Models {
+    internal class Book {
+        private string _title;
+        private int _publicationYear;
+        private Author _author;
+
+        public string Title { get => _title; set => _title = value; }
+
+        public int PublicationYear => _publicationYear;
+
+        public Author Author { get => _author; set => _author = value; }
+    }
+}
